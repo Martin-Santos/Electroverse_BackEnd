682dfcba85849535f55e96818b9d1d6b4f9580cb
Santos Curadelli Martin Ezequiel <me.santos@etec.local>
2023-10-25 13:05:17 -0300

    ejercicio 7 puntos

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,9 +1,51 @@
-import express, { Request, Response} from 'express';
+import express, {Request, Response} from 'express';
 
 const router = express.Router();
-
+let productoMercancia = [ 
+    {nombre: "Toyota", modelo:"GT86", precio:15000, paisOrigen:"Japon" },
+    {nombre: "Amd", modelo:"Ryzen 7", precio:50, paisOrigen:"China" },
+    {nombre: "Iphone", modelo:"15 Pro", precio:1200, paisOrigen:"USA" }
+]
+// 1
 router.get('/', (_: Request, res: Response) => {
-    res.send('Primer Server');
+    res.send(productoMercancia);
 })
+// 2
+router.get('/menoracien', (_: Request, res: Response) => {
+    res.send(productoMercancia.filter(productos => productos.precio < 100));
+    res.json(productoMercancia);
+})
+// 3
+// 4
+// 5
+router.get('/', (_: Request, res: Response) => {
+    const { pais } = req.params;
+    const productFound = productoMercancia.find(producto => producto.paisOrigen === pais);
+    if (productFound){
+        res.json(productFound);
+    } else{
+        res.status(404).send('Producto sin encontrar');
+    }
+};
+
+// 6
+router.get('/', (_: Request, res: Response) => {
+    const {precio} = req.params;
+    const productFound = productoMercancia.find(producto => producto.precio === Number(precio));
+    if (productFound) {
+        res.json(productFound);
+    }  else {
+        res.status(404).send('Producto sin encontrar');
+    }
+   
+})
+
+
+
+
+
+
+
+
 
 export default router;
