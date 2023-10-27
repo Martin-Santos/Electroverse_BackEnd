3d3a11e797ad9419ed39214f5da99cb518942dd2
Santos Curadelli Martin Ezequiel <me.santos@etec.local>
2023-10-27 12:49:20 -0300

    punto 1, 2, 4, 5 y 6 hechos, faltan el 3 y 7

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,24 +1,56 @@
 import express, {Request, Response} from 'express';
+const app = express();
+app.use(express.json());
 
 const router = express.Router();
 let productoMercancia = [ 
     {nombre: "Toyota", modelo:"GT86", precio:15000, paisOrigen:"Japon" },
-    {nombre: "Amd", modelo:"Ryzen 7", precio:50, paisOrigen:"China" },
-    {nombre: "Iphone", modelo:"15 Pro", precio:1200, paisOrigen:"USA" }
-]
+    {nombre: "Amd", modelo:"Ryzen7", precio:50, paisOrigen:"China" },
+    {nombre: "Iphone", modelo:"15Pro", precio:1200, paisOrigen:"USA" }
+];
 // 1
-router.get('/', (_: Request, res: Response) => {
+router.get('/products', (_: Request, res: Response) => {
     res.send(productoMercancia);
-})
+});
 // 2
-router.get('/menoracien', (_: Request, res: Response) => {
+router.get('/products/menoresacien', (_: Request, res: Response) => {
     res.send(productoMercancia.filter(productos => productos.precio < 100));
     res.json(productoMercancia);
-})
+});
 // 3
 // 4
+router.get('/products/:modelo', (req: Request, res: Response) => {
+    const {modelo} = req.params;
+    const longitud = productoMercancia.length;
+
+    productoMercancia = productoMercancia.filter(producto => producto.modelo !== modelo);
+    
+    if (productoMercancia.length === longitud) {
+        res.status(404).send('producto no encontrado');
+        console.log('producto no encontrado');
+    } else {
+        res.send('Producto eliminado');
+        console.log('Producto eliminado');
+    }
+});
+
+// app.get('/products/eliminar/:modelo', (req: Request, res: Response) => {
+//     const modelo = req.params.modelo;
+//     const indice = productoMercancia.findIndex((producto) => producto.modelo === modelo);
+  
+//     if (indice !== -1) {
+//       // Eliminar el producto
+//       productoMercancia.splice(indice, 1);
+//       res.send(`Producto con modelo ${modelo} eliminado.`);
+//     } else {
+//       res.status(404).send(`Producto con modelo ${modelo} no encontrado.`);
+//     }
+//   });
+
+
+
 // 5
-router.get('/', (_: Request, res: Response) => {
+router.get('/products/porpais/:pais', (req: Request, res: Response) => {
     const { pais } = req.params;
     const productFound = productoMercancia.find(producto => producto.paisOrigen === pais);
     if (productFound){
@@ -26,10 +58,10 @@ router.get('/', (_: Request, res: Response) => {
     } else{
         res.status(404).send('Producto sin encontrar');
     }
-};
+});
 
 // 6
-router.get('/', (_: Request, res: Response) => {
+router.get('/products/porprecio/:precio', (req: Request, res: Response) => {
     const {precio} = req.params;
     const productFound = productoMercancia.find(producto => producto.precio === Number(precio));
     if (productFound) {
@@ -38,8 +70,9 @@ router.get('/', (_: Request, res: Response) => {
         res.status(404).send('Producto sin encontrar');
     }
    
-})
+});
 
+//7
 
 
 
